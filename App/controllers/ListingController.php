04815b7dec6a4a81eaf9252989ba9cece982ff37
Aldrich Calungcagin <aldrichcalungcagin@gmail.com>
2026-05-16 11:55:28 +0800

    CRUD 2 Added

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -59,7 +59,7 @@
 
             $newListingData = array_map('sanitize', $newListingData);
 
-            $requiredFields = ['title', 'description', 'email', 'city', 'state'];
+            $requiredFields = ['title', 'description', 'salary', 'email', 'city', 'state'];
 
             $errors = [];
 
@@ -77,8 +77,59 @@
                 ]);
             } else {
                 //Submit data
-                echo "Success";
+                
+                // $this->db->query('INSERT INTO listings (title, description, salary, tags, company, address, city, state, phone, email, requirements, benefits, user_id) VALUES (:title, :description, :salary, :tags, :company, :address, :city, :state, :phone, :email, :requirements, :benefits, :user_id)', $newListingData);
+                
+                $fields = [];
+
+                foreach($newListingData as $field => $value) {
+                    $fields[] = $field;
+                }
+                $fields = implode(', ', $fields);
+
+                $value = [];
+
+                foreach($newListingData as $field => $value) {
+                    if($value === '') {
+                        $newListingData[$field] = null;
+                    }
+                    $values[] = ':' . $field;
+                }
+                $values = implode(', ', $values);
+
+                $query = "INSERT INTO listings ({$fields}) VALUES ({$values})";
+
+                $this->db->query($query, $newListingData);
+
+                redirect('/listings');
             }
+        }
+
+        /**
+         * Delete a listing
+         * 
+         * @param array $params
+         * @return void
+         */
+        public function destroy($params) {
+            $id = $params['id'];
+
+            $params = [
+                'id' => $id
+            ];
+
+            $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+            if(!$listing) {
+                ErrorController::notFound('Listing not found');
+                return;
+            }
+            $this->db->query('DELETE FROM listings WHERE id = :id', $params);
+
+            //Set flash message
+            $_SESSION['success_message'] = 'Listing deleted successfully';
+
+            redirect('/listings');
         }
     }
 ?>
--- framework/router.php
+++ framework/router.php
@@ -84,6 +84,12 @@ class Router {
     public function route($uri) {
         $requestMethod = $_SERVER['REQUEST_METHOD'];
 
+        //Check for _method input
+        if($requestMethod === 'POST' && isset($_POST['_method'])) {
+            // Override the request method with the value of _method
+            $requestMethod = strtoupper($_POST['_method']);
+        }
+
         foreach ($this->routes as $route) {
             //Split the current URI into segments
             $uriSegments = explode('/', trim($uri, '/'));
@@ -114,7 +120,7 @@ class Router {
                     $controllerMethod = $route['controllerMethod'];
 
                     $controllerInstance = new $controller();
-                    $controllerInstance->$controllerMethod();
+                    $controllerInstance->$controllerMethod($params);
                     return;
                 }
             }
@@ -122,5 +128,7 @@ class Router {
         
         ErrorController::notFound();
     }
+
+    
 }
 ?>
